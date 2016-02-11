8d5a368eb3f618eafb7712ad6fecf91e9d347e71
Grayson Hicks <graysonhicks@gmail.com>
2016-02-11 17:50:39 -0500

    complete

--- main.js
+++ main.js
@@ -1,60 +1,50 @@
 'use strict';
-//use setTime??? and setInterval??
+//use setTime??? and setInterval?
+
 // Time Variables
 var hours = document.querySelector(".hours");
 var minutes = document.querySelector(".minutes");
 var seconds = document.querySelector(".seconds");
-var milliseconds = document.querySelector(".milliseconds");
-
-// Button Variables
-var startButton = document.querySelector(".start");
-var stopButton = document.querySelector(".stop");
-var clearButton = document.querySelector(".clear");
-
-
-// Timer Variables
-var timerInterval; // must be set outside timer function so it can be cleared by stop button later
 
 // Functions
 
   //START BUTTON
-      function startButtonHandler(evt){
-        //define counter and set interval
-        timerInterval = setInterval(timer, 1);
-        var counter = 0;
-        //counter function and printing
-        function timer(){
-        //add to counter
-          counter += 1;
-          milliseconds.textContent = ("" + (Math.floor(counter))).slice(-2); // write times to html
-          seconds.textContent = ("00" + (Math.floor(counter / 100) % 60)).slice(-2); //gets lowest interval
-          minutes.textContent = ("00" + (Math.floor(counter / 6000) % 60)).slice(-2);// divide by proper time
-          hours.textContent = ("00" + (Math.floor(counter / 360000) % 60)).slice(-2);//increment since counter
-                                                                                  // runs every 1ms
-        }
-        //Log check and set interval
-        console.log("clicked");
+      function clockCounter(evt){
 
+        setInterval(timer, 1000);
+
+        function timer(){
+          var currentDate = new Date();
+         // write times to html
+          seconds.textContent = ("0" + currentDate.getSeconds()).slice(-2); //gets lowest interval
+          minutes.textContent = ("0" + currentDate.getMinutes()).slice(-2);// divide by proper time
+          hours.textContent = (currentDate.getHours() % 12);//increment since counter
+          console.log("check");                                                                    // runs every 1ms
+        }
+
+        setInterval(colorAndProgressMaker, 1000);
+
+        function colorAndProgressMaker() {
+          //grab body
+          var htmlBody = document.querySelector('.gradient');
+          //grab date
+          var currentDate = new Date;
+          //set date to percentages of totals
+          var hoursPercent = Math.floor((((currentDate.getHours()) / 24) * 100));
+          var minutePercent = Math.floor((((currentDate.getMinutes()) / 60) * 100));
+          var secondsPercent = Math.floor((((currentDate.getSeconds()) / 60) * 100));
+          //turn percentages in to hex numbers, adding an extra zero for 1 digit numbers and only keeping last two digits
+          var secondsString = ("0" + (secondsPercent).toString(16)).slice(-2);
+          var minutesString = ("0" + (minutePercent).toString(16)).slice(-2);
+          var hoursString = ("0" + (hoursPercent).toString(16)).slice(-2);
+          //concatenate hex values with hash in front
+          var backgroundHex = "#" + hoursString + minutesString + secondsString;
+          //insert hex value to background-gradient
+          htmlBody.style.background = "radial-gradient(circle at 50% 300px, #5a5c5c, "+backgroundHex+")";
+          var progressBar = document.querySelector('.progress-bar');
+          progressBar.style.width = secondsPercent + "%";
+        }
       }
         // ON START CLICK
-      startButton.addEventListener('click', startButtonHandler);
 
-      //STOP BUTTON
-      function stopButtonHandler(evt){
-          //stop timer interval, alert check
-          clearInterval( timerInterval );
-      }
-        //on start click
-      stopButton.addEventListener('click', stopButtonHandler);
-
-      // CLEAR BUTTON
-
-      function clearButtonHandler(){
-        //clear time to double zeros
-          hours.textContent = "00";
-          minutes.textContent = "00";
-          seconds.textContent = "00";
-          milliseconds.textContent = "00";
-      }
-      //on clear click
-      clearButton.addEventListener('click', clearButtonHandler);
+      clockCounter();
